0dba041c796b4854c783909aec420055736b9e7b
Seguya Nicholus <seguyanicholus2015@gmail.com>
2019-11-30 12:07:31 +0300

    Google strategy Setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,6 +5,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
     new GoogleStrategy({
         // Options for the google strategy
+        clientID:'667815717558-eiemd8t13c8cjs8naidb8u5qggdc6n46.apps.googleusercontent.com',
+        clientSecret:'4waREV35BwYQIwL1eutYLyoG'
 }), ()=>{
     // Passport callback function
 })
