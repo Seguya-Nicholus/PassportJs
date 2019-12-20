2d39ba8c6348c54ac0616edcd59b8aa5c2a93e90
Seguya Nicholus <seguyanicholus2015@gmail.com>
2019-12-21 02:58:32 +0300

    Saving User to local mongo Database

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys')
+const User = require('../modals/userModal')
 
 passport.use(
     new GoogleStrategy({
@@ -13,5 +14,11 @@ passport.use(
     // Passport callback function
     console.log('Passport callback function fired')
     console.log(profile)
+    new User({
+        username:profile.displayName,
+        googleId:profile.id
+    }).save().then((newUser)=>{
+        console.log('New User Created : ' + newUser);        
+    })
 })
 ) 
